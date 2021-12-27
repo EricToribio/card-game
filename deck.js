0e2340e55b0ba93bd08473039a50d58fce1792ec
Eric Toribio <erictoribio2010@gmail.com>
2021-12-27 03:12:22 -0500

    initial commit

---
+++ deck.js
@@ -0,0 +1,41 @@
+const SUITS = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+const VALUES = ['♥', '♣', '♦', '♠']
+
+
+export default class Deck{
+    constructor(cards = freshDeck()){
+        this.cards = cards
+    }
+
+    get numberOfCards(){
+        return this.cards.length
+    }
+    shuffle(){
+        // this.cards.sort(() => Math.random() - 0.5)
+        for(let i = this.numberOfCards - 1; i > 0; i--){
+            const newIndex = Math.floor(Math.random() * ( i  + 1))
+            const oldValue = this.cards[newIndex]
+            this.cards[newIndex] = this.cards[i]
+            this.cards[i] = oldValue
+        }
+    }
+}
+
+class Card{
+    constructor(suit,value){
+        this.suit = suit
+        this.value = value
+    }
+}
+
+function freshDeck(){
+    return SUITS.flatMap(suit =>{
+        return VALUES.map( value => {
+            return new Card(suit, value)
+        })
+    })
+}
+
+
+
+
